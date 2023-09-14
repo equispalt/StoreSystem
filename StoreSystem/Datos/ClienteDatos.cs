34932497c442b2f30625703c5188378a6819b62a
equispal <equispalt@gmail.com>
2023-09-13 21:43:13 -0600

    creacion de servicios
    ClienteDatos
    ProveedorDatos

---
+++ StoreSystem/Datos/ClienteDatos.cs
@@ -0,0 +1,182 @@
+﻿using StoreSystem.Models;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace StoreSystem.Datos
+{
+    public class ClienteDatos
+    {
+        private readonly IConfiguration _configuration;
+
+        public ClienteDatos(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public List<Clientes> Listar()
+        {
+            var oLista = new List<Clientes>();
+
+            try
+            {
+                using (var connectionString = new SqlConnection(_configuration.GetConnectionString("conexion")))
+                {
+                    connectionString.Open();
+                    SqlCommand cmd = new SqlCommand("SP_CliListar", connectionString);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    using (var dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            oLista.Add(new Clientes()
+                            {
+
+                                id_cliente = dr.GetInt32("id_cliente"),
+                                nombre_cliente = dr["nombre_cliente"].ToString(),
+                                apellido_cliente = dr["apellido_cliente"].ToString(),
+                                dpi_cliente = dr["dpi_cliente"].ToString(),
+                                nit_cliente = dr["nit_cliente"].ToString(),
+                                telefono_cliente = dr["telefono_cliente"].ToString(),
+                                direccion_cliente = dr["direccion_cliente"].ToString(),
+                                correo_cliente = dr["correo_cliente"].ToString()
+
+                            });
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+            }
+            return oLista;
+        }
+
+        public Clientes Buscar(int idCliente)
+        {
+            var oCliente = new Clientes();
+            try
+            {
+                using (var connectionString = new SqlConnection(_configuration.GetConnectionString("conexion")))
+                {
+                    connectionString.Open();
+                    SqlCommand cmd = new SqlCommand("SP_CliBuscarID", connectionString);
+                    cmd.Parameters.AddWithValue("@idCliente", idCliente);
+
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    using (var dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            oCliente.id_cliente = dr.GetInt32("id_cliente");
+                            oCliente.nombre_cliente = dr["nombre_cliente"].ToString();
+                            oCliente.apellido_cliente = dr["apellido_cliente"].ToString();
+                            oCliente.dpi_cliente = dr["dpi_cliente"].ToString();
+                            oCliente.nit_cliente = dr["nit_cliente"].ToString();
+                            oCliente.telefono_cliente = dr["telefono_cliente"].ToString();
+                            oCliente.direccion_cliente = dr["direccion_cliente"].ToString();
+                            oCliente.correo_cliente = dr["correo_cliente"].ToString();
+
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+            }
+            return oCliente;
+        }
+
+        public bool Crear(Clientes oCliente)
+        {
+            bool rpta;
+            try
+            {
+                using (var connectioString = new SqlConnection(_configuration.GetConnectionString("conexion")))
+                {
+                    connectioString.Open();
+                    SqlCommand cmd = new SqlCommand("SP_CliCrear", connectioString);
+
+                    cmd.Parameters.AddWithValue("@nombreCliente", oCliente.nombre_cliente);
+                    cmd.Parameters.AddWithValue("@apellidoCliente", oCliente.apellido_cliente);
+                    cmd.Parameters.AddWithValue("@dpiCliente", oCliente.dpi_cliente);
+                    cmd.Parameters.AddWithValue("@nitCliente", oCliente.nit_cliente);
+                    cmd.Parameters.AddWithValue("@telefonoCliente", oCliente.telefono_cliente);
+                    cmd.Parameters.AddWithValue("@direccionCliente", oCliente.direccion_cliente);
+                    cmd.Parameters.AddWithValue("@correoCliente", oCliente.correo_cliente);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                rpta = false;
+            }
+            return rpta;
+        }
+
+        public bool Editar(Clientes oCliente)
+        {
+            bool rpta;
+            try
+            {
+                using (var connectioString = new SqlConnection(_configuration.GetConnectionString("conexion")))
+                {
+                    connectioString.Open();
+                    SqlCommand cmd = new SqlCommand("SP_CliActualizar", connectioString);
+                    cmd.Parameters.AddWithValue("@idCliente", oCliente.id_cliente);
+                    cmd.Parameters.AddWithValue("@nombreCliente", oCliente.nombre_cliente);
+                    cmd.Parameters.AddWithValue("@apellidoCliente", oCliente.apellido_cliente);
+                    cmd.Parameters.AddWithValue("@nitCliente", oCliente.nit_cliente);
+                    cmd.Parameters.AddWithValue("@dpiCliente", oCliente.dpi_cliente);
+                    cmd.Parameters.AddWithValue("@telefonoCliente", oCliente.telefono_cliente);
+                    cmd.Parameters.AddWithValue("@direccionCliente", oCliente.direccion_cliente);
+                    cmd.Parameters.AddWithValue("@correoCliente", oCliente.correo_cliente);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                rpta = false;
+            }
+            return rpta;
+        }
+
+        public bool Eliminar(Clientes oCliente)
+        {
+            bool rpta;
+            try
+            {
+                using (var connectionString = new SqlConnection(_configuration.GetConnectionString("conexion")))
+                {
+                    connectionString.Open();
+                    SqlCommand cmd = new SqlCommand("SP_CliEliminar", connectionString);
+                    cmd.Parameters.AddWithValue("@idCliente", oCliente.id_cliente);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                rpta = false;
+            }
+            return rpta;
+        }
+
+
+    } //finaliza clase
+} // finaliza namespace
---
+++ StoreSystem/Datos/ProveedorDatos.cs
@@ -0,0 +1,171 @@
+﻿
+using Microsoft.Data.SqlClient;
+using StoreSystem.Models;
+using System.Data;
+
+namespace StoreSystem.Datos
+{
+    public class ProveedorDatos
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProveedorDatos(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public List<Proveedores> Listar()
+        {
+            var oLista = new List<Proveedores>();
+            try
+            {
+                using (var connectionString = new SqlConnection(_configuration.GetConnectionString("conexion")))
+                {
+                    connectionString.Open();
+                    SqlCommand cmd = new SqlCommand("SP_ProvListar", connectionString);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    using (var dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                            oLista.Add(new Proveedores()
+                            {
+                                id_proveedor = dr.GetInt32("id_proveedor"),
+                                nombre_proveedor = dr["nombre_proveedor"].ToString(),
+                                nit_proveedor = dr["nit_proveedor"].ToString(),
+                                telefono_proveedor = dr["telefono_proveedor"].ToString(),
+                                direccion_proveedor = dr["direccion_proveedor"].ToString(),
+                                correo_proveedor = dr["correo_proveedor"].ToString()
+
+                            });
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+            }
+            return oLista;
+        }
+
+        public Proveedores Buscar(int idProveedor)
+        {
+            var oProveedor = new Proveedores();
+
+            try
+            {
+                using (var connectionString = new SqlConnection(_configuration.GetConnectionString("conexion")))
+                {
+                    connectionString.Open();
+                    SqlCommand cmd = new SqlCommand("SP_ProvBuscarID", connectionString);
+                    cmd.Parameters.AddWithValue("@idProveedor", idProveedor);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    using (var dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            oProveedor.id_proveedor = dr.GetInt32("id_proveedor");
+                            oProveedor.nombre_proveedor = dr["nombre_proveedor"].ToString();
+                            oProveedor.nit_proveedor = dr["nit_proveedor"].ToString();
+                            oProveedor.telefono_proveedor = dr["telefono_proveedor"].ToString();
+                            oProveedor.direccion_proveedor = dr["direccion_proveedor"].ToString();
+                            oProveedor.correo_proveedor = dr["correo_proveedor"].ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+            }
+
+            return oProveedor;
+        }
+
+        public bool Crear(Proveedores oProveedor)
+        {
+            bool rpta;
+            try
+            {
+                using (var connectionString = new SqlConnection(_configuration.GetConnectionString("conexion")))
+                {
+                    connectionString.Open();
+                    SqlCommand cmd = new SqlCommand("SP_ProvCrear", connectionString);
+
+                    cmd.Parameters.AddWithValue("@nombreProveedor", oProveedor.nombre_proveedor);
+                    cmd.Parameters.AddWithValue("@nitProveedor", oProveedor.nit_proveedor);
+                    cmd.Parameters.AddWithValue("@telefonoProveedor", oProveedor.telefono_proveedor);
+                    cmd.Parameters.AddWithValue("@direccionProveedor", oProveedor.direccion_proveedor);
+                    cmd.Parameters.AddWithValue("@correoProveedor", oProveedor.correo_proveedor);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                rpta = false;
+            }
+            return rpta;
+        }
+
+        public bool Editar(Proveedores oProveedor)
+        {
+            bool rpta;
+            try
+            {
+                using (var connectionString = new SqlConnection(_configuration.GetConnectionString("conexion")))
+                {
+                    connectionString.Open();
+                    SqlCommand cmd = new SqlCommand("SP_ProvActualizar", connectionString);
+                    cmd.Parameters.AddWithValue("@idProveedor", oProveedor.id_proveedor);
+                    cmd.Parameters.AddWithValue("@nombreProveedor", oProveedor.nombre_proveedor);
+                    cmd.Parameters.AddWithValue("@nitProveedor", oProveedor.nit_proveedor);
+                    cmd.Parameters.AddWithValue("@telefonoProveedor", oProveedor.telefono_proveedor);
+                    cmd.Parameters.AddWithValue("@direccionProveedor", oProveedor.direccion_proveedor);
+                    cmd.Parameters.AddWithValue("@correoProveedor", oProveedor.correo_proveedor);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                rpta = false;
+            }
+
+            return rpta;
+        }
+
+        public bool Eliminar(Proveedores oProveedor)
+        {
+            bool rpta;
+            try
+            {
+                using (var connectionString = new SqlConnection(_configuration.GetConnectionString("conexion")))
+                {
+                    connectionString.Open();
+                    SqlCommand cmd = new SqlCommand("SP_ProvEliminar", connectionString);
+                    cmd.Parameters.AddWithValue("@idProveedor", oProveedor.id_proveedor);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                rpta = false;
+            }
+
+            return rpta;
+        }
+
+    }
+}
