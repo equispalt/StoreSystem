9d90ac9353523477d9ae398e8e9f26b925937c07
equispal <equispalt@gmail.com>
2023-09-24 18:52:07 -0600

    clase categoriaProductosDatos, funcion Buscar

--- StoreSystem/Datos/CategoriaProductosDatos.cs
+++ StoreSystem/Datos/CategoriaProductosDatos.cs
@@ -43,5 +43,36 @@ namespace StoreSystem.Datos
             return oLista;
         }
 
+        public CategoriaProductos Buscar(int idCategoria) 
+        {
+            var oCategoria = new CategoriaProductos();
+            try
+            {
+                using (var connectionString = new SqlConnection(_configuration.GetConnectionString("conexion")))
+                {
+                    connectionString.Open();
+                    SqlCommand cmd = new SqlCommand("SP_CatBuscarID", connectionString);
+                    cmd.Parameters.AddWithValue("@idCategoria", idCategoria);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (var dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            oCategoria.id_categoria = dr.GetInt32("id_categoria");
+                            oCategoria.nombre_categoria = dr["nombre_categoria"].ToString();
+                            oCategoria.descripcion_categoria = dr["descripcion_categoria"].ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+            }
+        return oCategoria;  
+        }
+
+
     }
 }
