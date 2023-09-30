5260f94eaa6bf84f51ccc50641041aab5801f680
equispal <equispalt@gmail.com>
2023-09-30 00:26:00 -0600

    cambios a navar, colores, funcion de sombreado

---
+++ StoreSystem/Controllers/ExistenciasController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreSystem.Models;
+
+namespace StoreSystem.Controllers
+{
+    public class ExistenciasController : Controller
+    {
+        public IActionResult MostrarExistencias()
+        {
+            return View();
+        }
+    }
+}
