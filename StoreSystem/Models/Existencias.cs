dedccfae7deceab9b388a236722d89105d54a668
equispal <equispalt@gmail.com>
2023-10-03 18:17:04 -0600

    creacion de modelo existencias

--- StoreSystem/Models/Existencias.cs
+++ StoreSystem/Models/Existencias.cs
@@ -2,5 +2,10 @@
 {
     public class Existencias
     {
+        public int id_existencia { get; set; } 
+        public string producto_id { get; set; }
+        public string cantidad_existencia { get; set; } 
+        public string FechaUltimaActualizacion { get; set; }    
+
     }
 }
