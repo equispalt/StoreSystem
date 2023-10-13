877ac6262f1f66abe967245389674a95e71f54a9
equispal <equispalt@gmail.com>
2023-10-13 00:44:37 -0600

    mvc listar de precio producto

---
+++ StoreSystem/Controllers/PrecioProductosController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreSystem.Datos;
+
+namespace StoreSystem.Controllers
+{
+    public class PrecioProductosController : Controller
+    {
+        public PrecioProductosDatos _PrecioProductosDatos;
+        public PrecioProductosController(IConfiguration _configuration)
+        {
+            _PrecioProductosDatos = new PrecioProductosDatos(_configuration);
+        }
+        public IActionResult Listar()
+        {
+            var oLista = _PrecioProductosDatos.Listar();   
+            return View(oLista);
+        }
+    }
+}
--- StoreSystem/Datos/PrecioProductosDatos.cs
+++ StoreSystem/Datos/PrecioProductosDatos.cs
@@ -1,6 +1,52 @@
-﻿namespace StoreSystem.Datos
+﻿using Microsoft.Data.SqlClient;
+using StoreSystem.Models;
+using System.Data;
+
+namespace StoreSystem.Datos
 {
     public class PrecioProductosDatos
     {
+        private readonly IConfiguration _configuration;
+
+        public PrecioProductosDatos(IConfiguration configuration)
+        {
+            _configuration = configuration;        
+        }
+
+        public List<PrecioProductos> Listar()
+        {
+            var oLista = new List<PrecioProductos>();
+            try
+            {
+                using (var connectionString = new SqlConnection(_configuration.GetConnectionString("conexion")))
+                {
+                    connectionString.Open();
+                    SqlCommand cmd = new SqlCommand("[SP_PreListar]", connectionString);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    using (var dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                            oLista.Add(new PrecioProductos()
+                            {
+                                id_precio = dr.GetInt32("id_precio"),
+                                producto_id = dr.GetInt32("producto_id"),
+                                nombre_producto = dr["nombre_producto"].ToString(),
+                                precio_unidad = dr.GetFloat("precio_unidad"),
+                                fecha_vigencia = dr.GetDateTime("fecha_vigencia"),
+                                fecha_configuracion = dr.GetDateTime("fecha_configuracion"),
+                                correoUsr = dr["correoUsr"].ToString(),
+                                estado = dr.GetInt32("estado")
+                            });
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+            }
+            return oLista;
+        }
+
     }
 }
