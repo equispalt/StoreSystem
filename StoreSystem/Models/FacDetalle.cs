6c2921a23d3423a2ffd10d73118e6b52d270e9b3
equispal <equispalt@gmail.com>
2023-10-15 11:08:53 -0600

    creacion de modelo facturar, encabezado y detalle

---
+++ StoreSystem/Models/FacDetalle.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Principal;
+
+namespace StoreSystem.Models
+{
+    public class FacDetalle
+    {
+        public int id_detalle {  get; set; }   
+        public int factura_id { get; set; }
+        public int producto_id { get; set; }    
+        public float cantidad_detalle { get; set; }
+        public int precio_id { get; set; }  
+        public float precio_detalle { get; set; }
+        public float subtotal_detalle { get; set; }
+    }
+}
---
+++ StoreSystem/Models/FacEncabezado.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Principal;
+
+namespace StoreSystem.Models
+{
+    public class FacEncabezado
+    {
+        public int id_factura { get; set; }
+        public DateTime fecha_factura { get; set; }
+        public string estado_factura { get; set; }
+        public int cliente_id { get; set; }
+        public int usuario_id { get; set; }
+        public float total_factura { get; set; }
+);
+    }
+}
